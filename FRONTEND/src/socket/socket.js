1725962feef02010e42321ada26d418af0c3a7ae
Yash Chohtel <yashchohtel@gmail.com>
2026-02-22 23:50:52 +0530

    frontend connection added

--- FRONTEND/src/socket/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-// your backend URL
-const URL = import.meta.env.VITE_BACKEND_URL;
-
-// initialize and export the Socket.IO client with the backend URL and CORS settings
-export const socket = io(URL, {
-    withCredentials: true,
-});
