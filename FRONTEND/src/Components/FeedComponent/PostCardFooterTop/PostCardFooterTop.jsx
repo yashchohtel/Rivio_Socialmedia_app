bc0fbd62e9d5247417018f6f8d82e5e34cef181e
Yash Chohtel <yashchohtel@gmail.com>
2026-03-12 23:40:09 +0530

    COMMENT AND REPLY LIKE FEATURE STARETD

--- FRONTEND/src/Components/FeedComponent/PostCardFooterTop/PostCardFooterTop.jsx
+++ FRONTEND/src/Components/FeedComponent/PostCardFooterTop/PostCardFooterTop.jsx
@@ -112,6 +112,8 @@ const PostCardFooterTop = (props) => {
 
                     </div>
 
+                    
+
                     {/* comment section */}
                     <div
                         className="commentSection iconCountCont"
