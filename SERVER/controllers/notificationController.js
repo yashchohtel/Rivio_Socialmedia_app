014db06d36135de0cab5ab0ca88451d60ce64388
Yash Chohtel <yashchohtel@gmail.com>
2026-03-19 22:44:33 +0530

    get all notification controller created

--- SERVER/controllers/notificationController.js
+++ SERVER/controllers/notificationController.js
@@ -16,8 +16,49 @@ export const getAllNotifications = async (req, res, next) => {
         .populate("post", "images")
         .populate("comment", "text replies")
 
-    console.log("------------------");
-    console.log(notifications);
+
+    // optimized notifications
+    const optimizedNotificationData = notifications.map(notification => ({
+
+        _id: notification._id, // notificatio id
+        type: notification.type, // notification type
+
+        // optimized recipient user object for response
+        recipient: {
+            id: notification.recipient._id,
+            username: notification.recipient.username,
+            fullName: notification.recipient.fullName,
+            profileImage: notification.recipient.profileImage || null,
+            isVerified: notification.recipient.isVerified,
+            isPrivate: notification.recipient.isPrivate,
+
+            followersCount: notification.recipient.followers?.length || 0,
+            followingCount: notification.recipient.following?.length || 0,
+            postsCount: notification.recipient.posts?.length || 0,
+            bookmarksCount: notification.recipient.bookmarks?.length || 0,
+        },
+
+        // optimized sender user object for response
+        sender: {
+            id: notification.sender._id,
+            username: notification.sender.username,
+            fullName: notification.sender.fullName,
+            profileImage: notification.sender.profileImage || null,
+            isVerified: notification.sender.isVerified,
+            isPrivate: notification.sender.isPrivate,
+
+            followersCount: notification.sender.followers?.length || 0,
+            followingCount: notification.sender.following?.length || 0,
+            postsCount: notification.sender.posts?.length || 0,
+            bookmarksCount: notification.sender.bookmarks?.length || 0,
+
+        },
+
+        comment: notification.comment?.text, // comment text
+        post: notification.post?._id, // post id on which comment id made
+        isRead: notification.isRead, // is read flag
+        createdAt: notification.createdAt, // created
+    }))
 
 
     // return response
