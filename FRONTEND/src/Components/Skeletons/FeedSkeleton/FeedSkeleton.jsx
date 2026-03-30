132960eef96c371b1e9268c4fe26862516e4400a
Yash Chohtel <yashchohtel@gmail.com>
2026-03-30 23:20:41 +0530

    post document normilization  feature started

--- FRONTEND/src/Components/Skeletons/FeedSkeleton/FeedSkeleton.jsx
+++ FRONTEND/src/Components/Skeletons/FeedSkeleton/FeedSkeleton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./feedSkeleton.css"
 
 const FeedSkeleton = () => {
+    
     return (
         <>
             {/* post skeleton skeleton */}
--- FRONTEND/src/pages/SinglePost/SinglePost.jsx
+++ FRONTEND/src/pages/SinglePost/SinglePost.jsx
@@ -6,6 +6,7 @@ import { getSinglePost } from '../../features/posts/postThunk';
 import { useDispatch, useSelector } from 'react-redux';
 import PostCard from '../../Components/FeedComponent/PostCard/PostCard';
 import AccountSuggestion from '../../Components/AccountSuggestion/AccountSuggestion';
+import FeedSkeleton from '../../Components/Skeletons/FeedSkeleton/FeedSkeleton';
 
 const SinglePost = () => {
 
@@ -33,11 +34,41 @@ const SinglePost = () => {
 
     /* -------------------------------------- */
 
+    useEffect(() => {
+        console.log("singlePost state:", singlePost);
+        console.log("singlePost state isliked:", singlePost?.isLiked);
+    }, [singlePost]);
+
     // post card skeleton loading
-    if (singlePostLoading) return <p>Loading...</p>;
+    if (singlePostLoading) {
+
+        return (
+
+            <div className="singePostPage">
+
+                <div className="singlePostContentCont">
+
+                    <div className="postCardCont">
+                        <FeedSkeleton />
+                    </div>
+
+                    <div className="accountSuggestionCont">
+                        <AccountSuggestion />
+                    </div>
+
+                </div>
+
+            </div>
+
+        );
+    }
 
     // if single post is null do not render component
     if (!singlePost) return null;
+
+    /* -------------------------------------- */
+
+
 
     return (
 
